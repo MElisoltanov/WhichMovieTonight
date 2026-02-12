c0dbe27ccd945ac77a2878f4e3548207014468f8
flor4 <salanson.flora@outlook.fr>
2026-02-12 10:05:33 +0100

    fix: django module path

--- backend/apps/ratings/apps.py
+++ backend/apps/ratings/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class RatingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ratings'
+    name = 'apps.ratings'
     verbose_name = 'Ratings'
 
